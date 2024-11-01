6c7b4ac743e2c4ba2f83ec5b0a057d979f89280f
Vista1967 <stitar@gmail.com>
2024-11-01 21:28:13 +0100

    Set default city to Brussels

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 const apiKey = "959d9451d7f23817254ee887e8245961"; // Zamenite sa svojim OpenWeatherMap API ključem
 
+
 // Lista gradova za izbor
 const europeanCities = [
-    "Belgrade, RS", "Paris, FR", "London, GB", "Berlin, DE", "Madrid, ES", 
-    "Rome, IT", "Vienna, AT", "Athens, GR", "Budapest, HU", "Prague, CZ", 
-    "Warsaw, PL", "Dublin, IE", "Brussels, BE", "Amsterdam, NL", 
-    "Lisbon, PT", "Zurich, CH", "Stockholm, SE", "Oslo, NO", 
-    "Copenhagen, DK", "Helsinki, FI", "Sofia, BG", "Bucharest, RO", 
-    "Ljubljana, SI", "Zagreb, HR", "Podgorica, ME", "Sarajevo, BA", 
-    "Skopje, MK", "Tirana, AL", "Sint-Pieters-Leeuw, BE", "Šabac, RS"
+    "Belgrade", "Paris", "London", "Berlin", "Madrid", "Rome", "Vienna", "Athens", "Budapest", 
+    "Prague", "Warsaw", "Dublin", "Brussels", "Amsterdam", "Lisbon", "Zurich", "Stockholm", 
+    "Oslo", "Copenhagen", "Helsinki", "Sofia", "Bucharest", "Ljubljana", "Zagreb", "Podgorica", 
+    "Sarajevo", "Skopje", "Tirana", "Sint-Pieters-Leeuw, BE", "Šabac, RS"
 ];
 
 // Popunjavanje select elementa sa gradovima
 const citySelect = document.getElementById("city-select");
 europeanCities.forEach(city => {
     const option = document.createElement("option");
-    option.value = city; // Koristimo naziv grada sa oznakom zemlje
+    option.value = city;
     option.textContent = city;
     citySelect.appendChild(option);
 });
@@ -23,7 +21,8 @@ europeanCities.forEach(city => {
 // Funkcija za ažuriranje podataka o vremenu
 function updateWeather(city) {
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&appid=${apiKey}&units=metric&lang=fr`;
-    
+    console.log("API URL:", apiUrl); // Log URL za debug
+
     fetch(apiUrl)
         .then(response => {
             if (!response.ok) {
@@ -55,6 +54,7 @@ function refreshWeather() {
 citySelect.addEventListener("change", refreshWeather);
 
 // Postavljanje podrazumevanog grada
-const defaultCity = "Sint-Pieters-Leeuw, BE"; // Podrazumevani grad sa oznakom zemlje
+const defaultCity = "Brussels, BE"; // Podrazumevani grad
 citySelect.value = defaultCity; // Postavljanje izabranog grada
 refreshWeather(); // Ažuriranje podataka za podrazumevani grad
+
